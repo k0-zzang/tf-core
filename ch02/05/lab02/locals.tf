1a104d32e81aa4092d426d26ea51602291b297ca
k0-zzang <hyuna9582@gmail.com>
2026-05-13 14:39:38 +0900

    terraform: update datasources and locals

--- ch02/05/lab02/locals.tf
+++ ch02/05/lab02/locals.tf
@@ -1,12 +1,12 @@
 locals {
     project = "tf-core-lab01"
 
-    vpc_id = data.aws_vpc.default
+    vpc_id = data.aws_vpc.default.id
 
     instance = {
         name = "web"
 
-         ami                         = data.aws_ami.amazon_linux
+         ami                         = data.aws_ami.amazon_linux.id
          instance_type               = var.instance_type
          associate_public_ip_address = true
          subnet_id                   = data.aws_subnets.default.ids[0]
@@ -21,7 +21,7 @@ locals {
      name = "instance"
      
      assume_role_policy = data.aws_iam_policy_document.ec2_assume_role
-     policy_arn         = data.aws_iam_policy.aws_ssm_core
+     policy_arn         = data.aws_iam_policy.aws_ssm_core_policy.arn
     }
 }
     
